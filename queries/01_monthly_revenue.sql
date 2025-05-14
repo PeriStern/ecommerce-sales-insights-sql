8d2f8b99cdd457a7128ae3505a2aedae25cff7c2
Peri Stern <peristern15@gmail.com>
2025-05-14 16:26:05 +0100

    first query

--- queries/01_monthly_revenue.sql
+++ queries/01_monthly_revenue.sql
@@ -1,2 +1,9 @@
 --What is the total revenue earned each month?
+--join by OrderID
 SELECT 
+  strftime('%Y-%m', Orders.OrderDate) AS month,
+  ROUND(SUM("Order Details".UnitPrice * "Order Details".Quantity), 2) AS total_revenue
+FROM Orders
+JOIN "Order Details" ON Orders.OrderID = "Order Details".OrderID
+GROUP BY month
+ORDER BY month;
